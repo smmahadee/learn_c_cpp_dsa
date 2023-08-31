9bd556b6b54e71e97a85503ce89f0ac984c3eedc
smmahadee <smmahadee0@gmaiil.com>
2023-09-01 00:06:37 +0600

    max subarray sum - sliding window solution

--- problem-solving-pattern/sliding-window-pattern/maxSubarraySum.js
+++ problem-solving-pattern/sliding-window-pattern/maxSubarraySum.js
@@ -4,21 +4,41 @@
 // ex : [1, 5, 9, 2, 6, 9, 3], 3 = 18
 
 // NAIVE SOLUTION
-const maxSubarraySum = (array, num) => {
-  let result = -Infinity;
+// const maxSubarraySum = (array, num) => {
+//   let result = -Infinity;
 
-  for (let i = 0; i < array.length - num + 1; i++) {
-    let temp = 0;
-    for (let j = 0; j < num; j++) {
-      temp += array[i + j];
-    }
-    if (temp > result) {
-      result = temp;
-    }
+//   for (let i = 0; i < array.length - num + 1; i++) {
+//     let temp = 0;
+//     for (let j = 0; j < num; j++) {
+//       temp += array[i + j];
+//     }
+//     if (temp > result) {
+//       result = temp;
+//     }
+//   }
+
+//   return result;
+// };
+
+// SLIDING WINDOW SOLUTION
+const maxSubarraySum = (arr, num) => {
+  if (num > arr.length) return;
+
+  let max = 0;
+  let temp = 0;
+
+  for (let i = 0; i < num; i++) {
+    max += arr[i];
+  }
+  temp = max;
+
+  for (let i = num; i < arr.length; i++) {
+   temp  = temp - arr[i - num] + arr[i]
+   max = Math.max(max , temp);
   }
 
-  return result;
+  return max;
 };
 
-const result = maxSubarraySum([1, 5, 9, 2, 6, 9, 3], 3);
+const result = maxSubarraySum([3, 5, 2, 8], 2);
 console.log(result);

431361af478d72cd81cd615064e70daed4715415
smmahadee <smmmahadee0@gmail.com>
2023-10-02 09:52:36 +0600

    data types & variables in c

---
+++ module-1/hello-world.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Type your input - ");
+    scanf("%d", &number);
+    printf("your input value is %d\n", number);
+    return 0;
+}
+
+// hello
+
+// int main()
+// {
+//     int age = 20;
+//     float money = 20.5;
+//     printf("hello! \n");
+//     printf("I am a programmer,\nMy age is %d", age);
+//     printf("my bank balance is %0.2f", money);
+//     return 0;
+// }˜

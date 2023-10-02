964dc2503ab593e561222a5bc16930a7291759df
smmahadee <smmahadee0@gmail.com>
2023-10-02 21:42:26 +0600

    practice module 1

---
+++ module-1/practice.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    long long int num = 1000000000000000;
+    double num2 = 2343452342.555756777;
+    char character = 'A';
+
+    printf("your number is - %lld\nYour float value is - %lf\nYour character is - %c\n", num, num2, character);
+    return 0;
+}
+
+// int main() {
+//     int num1, num2, num3;
+//     char a;
+//     printf("Please provide 3 value - ");
+//     scanf("%d%c %d%c %d%c", &num1, &a, &num2, &a, &num3, &a);
+//     printf("Your 3 value is - %d,%d,%d\n", num1, num2, num3);
+//     return 0;
+// }
+
